d96d0f8c163a1d1993b3137698cd84b75af06b79
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-10-25 16:05:47 +0200

    Added several base events.

---
+++ src/com/sstengine/event/events/ChangeObstacleTileEvent.java
@@ -0,0 +1,48 @@
+package com.sstengine.event.events;
+
+import com.sstengine.map.tile.Tile;
+import com.sstengine.map.tileobject.Obstacle;
+
+/**
+ * The ChangeObstacleTileEvent is an event that changes the location of a given obstacle to a new Tile.
+ * The newTile can be set to null in order to remove a Obstacle from the map.
+ * @author Oscar de Leeuw
+ */
+public class ChangeObstacleTileEvent extends AbstractEvent {
+    private Obstacle obstacle;
+    private Tile newTile;
+    private Tile oldTile;
+
+    public ChangeObstacleTileEvent(Obstacle obstacle, Tile newTile) {
+        this.obstacle = obstacle;
+        this.newTile = newTile;
+        this.oldTile = obstacle.getTile();
+    }
+
+    /**
+     * Gets the obstacle that is involved with this event.
+     *
+     * @return The obstacle object.
+     */
+    public Obstacle getObstacle() {
+        return obstacle;
+    }
+
+    /**
+     * Gets the new tile of the obstacle.
+     *
+     * @return The tile the obstacle should be moved to.
+     */
+    public Tile getNewTile() {
+        return newTile;
+    }
+
+    /**
+     * Gets the tile the obstacle was at at the time of event creation.
+     *
+     * @return The old tile the obstacle.
+     */
+    public Tile getOldTile() {
+        return oldTile;
+    }
+}
---
+++ src/com/sstengine/event/events/ChangePlayerEntityStateEvent.java
@@ -0,0 +1,29 @@
+package com.sstengine.event.events;
+
+import com.sstengine.player.playerentity.PlayerEntity;
+import com.sstengine.state.State;
+
+/**
+ * The ChangePlayerEntityStateEvent changes the {@link State} of a PlayerEntity to the given state.
+ *
+ * @author Oscar de Leeuw
+ */
+public class ChangePlayerEntityStateEvent extends AbstractEvent {
+    private PlayerEntity player;
+    private State newState;
+    private State oldState;
+
+    public ChangePlayerEntityStateEvent(PlayerEntity player, State newState) {
+        this.player = player;
+        this.newState = newState;
+        //this.oldState = player.getState(); //TODO Give PlayerEntity a state.
+    }
+
+    public PlayerEntity getPlayer() {
+        return player;
+    }
+
+    public State getNewState() {
+        return newState;
+    }
+}
