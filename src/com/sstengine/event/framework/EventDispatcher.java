8a41a8c31d34be743fe4db210d536f76550af2b9
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-10-25 13:38:22 +0200

    Added event system framework.

---
+++ src/com/sstengine/event/framework/EventDispatcher.java
@@ -0,0 +1,63 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ **/
+
+package com.sstengine.event.framework;
+
+
+import com.sstengine.Game;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handles the routing of {@link Event} messages to associated handlers.
+ * A {@link HashMap} is used to store the association between events and their respective handlers.
+ *
+ * @author Ilkka Seppälä
+ * @author Oscar de Leeuw
+ */
+public class EventDispatcher {
+
+    private Map<Class<? extends Event>, Handler<? extends Event>> handlers;
+
+    public EventDispatcher() {
+        handlers = new HashMap<>();
+    }
+
+    /**
+     * Links an {@link Event} to a specific {@link Handler}.
+     *
+     * @param eventType The {@link Event} to be registered
+     * @param handler   The {@link Handler} that will be handling the {@link Event}
+     */
+    public <E extends Event> void registerHandler(Class<E> eventType,
+                                                  Handler<E> handler) {
+        handlers.put(eventType, handler);
+    }
+
+    /**
+     * Dispatches an {@link Event} depending on it's type.
+     *
+     * @param event The {@link Event} to be dispatched
+     */
+    @SuppressWarnings("unchecked")
+    public <E extends Event> void dispatch(E event, Game game) {
+        Handler<E> handler = (Handler<E>) handlers.get(event.getClass());
+        if (handler != null) {
+            handler.onEvent(event, game);
+        }
+    }
+
+}
