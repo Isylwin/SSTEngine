04c3b10e8fd8836a5cbae745a357966e1f6115bb
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-11-20 00:43:55 +0100

    Added more definition to the graphics classes.

--- src/com/sstengine/component/graphics/Graphics.java
+++ src/com/sstengine/component/graphics/Graphics.java
@@ -1,8 +1,20 @@
 package com.sstengine.component.graphics;
 
+import java.awt.*;
+
 /**
+ * The graphics interface is a contract for all objects that require a graphical representation.
+ *
  * @author Oscar de Leeuw
  */
 public interface Graphics {
-    void render();
+    /**
+     * Renders the object on the given painter on the screen.
+     *
+     * @param painter  The painter which supports the drawing.
+     * @param location The location at which the graphics should be drawn, in pixels.
+     * @param width    The maximum width that can be used for drawing the object, in pixels.
+     * @param height   The maximum height that can be used for drawing the object, in pixels.
+     */
+    void render(Painter painter, Point location, int width, int height);
 }
--- src/com/sstengine/component/graphics/GraphicsComponent.java
+++ src/com/sstengine/component/graphics/GraphicsComponent.java
@@ -1,11 +1,12 @@
 package com.sstengine.component.graphics;
 
+import java.awt.*;
+
 /**
  * The GraphicsComponent class
- * Every Object that uses a GraphicsComponent should implement the {@link Graphics} interface.
  *
  * @author Oscar de Leeuw
  */
 public abstract class GraphicsComponent {
-    public abstract void render(Graphics caller);
+    public abstract void render(Object caller, Painter painter, Point location, int width, int height);
 }
