fc69967a61e0ecf32fd1c9590c2dbfe9740c8b8c
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-11-22 09:57:10 +0100

    Added OrdinalDirection enum. Changed CardinalDirection JavaDoc.

--- src/com/sstengine/util/enumeration/CardinalDirection.java
+++ src/com/sstengine/util/enumeration/CardinalDirection.java
@@ -3,16 +3,19 @@ package com.sstengine.util.enumeration;
 import java.awt.*;
 
 /**
+ * The CardinalDirection enum holds all the cardinal direction (E, W, N, S).
+ * Contains a method to convert a cardinal direction to a point.
+ *
  * @author Oscar de Leeuw
  */
-public enum Direction {
-    EAST, WEST, NORTH, SOUTH;
+public enum CardinalDirection {
+    NORTH, SOUTH, WEST, EAST;
 
     static {
-        EAST.cartesian = new Point(1, 0);
-        WEST.cartesian = new Point(-1, 0);
         NORTH.cartesian = new Point(0, -1);
         SOUTH.cartesian = new Point(0, 1);
+        EAST.cartesian = new Point(1, 0);
+        WEST.cartesian = new Point(-1, 0);
     }
 
     private Point cartesian;
