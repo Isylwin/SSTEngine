70160e9349f0502b6ad506597e6b310ba7c3f8b9
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-11-24 16:57:31 +0100

    Fixed Team javadoc.

--- src/com/sstengine/team/Team.java
+++ src/com/sstengine/team/Team.java
@@ -9,7 +9,11 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * A team consists of a team name, a score and a list of teamMembers.
+ * The Team class represents a team within the {@link com.sstengine.Game}.
+ * Every {@link com.sstengine.player.Player} in the Game must belong to a team.
+ *
+ * A Team consists of a {@link Country} and a score.
+ * A Team can only have one {@link Leader} but it can have multiple {@link PlayerEntity}.
  *
  * @author Oscar de Leeuw
  */
@@ -58,18 +62,37 @@ public class Team {
         return country;
     }
 
+    /**
+     * Gets the leader of this Team.
+     * A team can only have one leader.
+     *
+     * @return The leader of the Team.
+     */
     public Leader getLeader() {
         return leader;
     }
 
+    /**
+     * Sets the leader of this Team.
+     *
+     * @param leader The leader that should be the leader of this Team.
+     */
     public void setLeader(Leader leader) {
         this.leader = leader;
     }
 
+    /**
+     * Gets all the PlayerEntities that are associated with this team.
+     * @return A List of all the PlayerEntities that belong to this team.
+     */
     public List<PlayerEntity> getPlayerEntities() {
         return Collections.unmodifiableList(playerEntities);
     }
 
+    /**
+     * Adds a given PlayerEntity to the Team if the Team doesn't already contain a PlayerEntity.
+     * @param entity The PlayerEntity that should be added to the Team.
+     */
     public void addPlayerEntity(PlayerEntity entity) {
         if (!playerEntities.contains(entity)) {
             playerEntities.add(entity);
